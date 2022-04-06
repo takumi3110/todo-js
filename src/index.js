10d2e3a3679b8b86a62b8b55a7d11c1c78b8c20f
takumi3110 <57779205+takumi3110@users.noreply.github.com>
2022-04-06 12:05:09 +0900

    要素を追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // text boxを取得して初期化
+  const addText = document.querySelector("#addText");
+  const inputText = addText.value;
+  addText.value = "";
+
+  // div
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // li
+  const li = document.createElement("li");
+
+  // liタグの小要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+  console.log(li);
+
+  // 未完了のリストに追加
+  document.querySelector("#incomplete-list").appendChild(li);
+};
+
+const addButton = document.querySelector("#addButton");
+addButton.addEventListener("click", () => onClickAdd());
